094b4239e1930c6a3615b78c3af98e68894915e4
pavel <sumanoop@gmail.com>
2019-09-02 17:32:47 -0500

    Logo and Fade Feature

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,10 +1,24 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-`
+  ${props => props.fixed && css`
+  {
+    background: #fff;
+    border-radius: 60px;
+    border-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    left:0;
+    margin: 0 auto;
+    padding: 5px;
+    position: fixed;
+    rigth: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+  }`
+}`
 
 export const Item = styled.li`
   padding: 0 8px;
