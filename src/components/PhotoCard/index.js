20a65f63579a7f829875d68d6089f3803ab92730
pavel <sumanoop@gmail.com>
2019-09-02 20:06:40 -0500

    Intersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { ImgWrapper, Img, Button } from './styles';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { Article, ImgWrapper, Img, Button } from './styles';
 import { MdFavoriteBorder } from"react-icons/md";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg';
 
 export const PhotoCard = ({ id, likes=0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] =  useState(false)
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(function(entries)
+    {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log(' si ' )
+        setShow(true)
+        observer.disconnect()
+      }
+      
+    })
+    observer.observe(element.current)
+    }, [element])
+     
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
+    <Article ref = {element}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
         <ImgWrapper>
           <Img src = {src} />
         </ImgWrapper>
@@ -15,6 +35,9 @@ export const PhotoCard = ({ id, likes=0, src = DEFAULT_IMAGE }) => {
       <Button>
       <MdFavoriteBorder size='32' /> {likes} likes!
       </Button>
-    </article>
+        </Fragment>
+      }
+      
+    </Article>
   )
 }
