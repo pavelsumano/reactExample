b67e518a013d9ce22e84026befe2b68ab7bbc6dc
pavel <sumanoop@gmail.com>
2019-09-02 20:27:24 -0500

    Polyfill use

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,43 +1,45 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { Article, ImgWrapper, Img, Button } from './styles';
-import { MdFavoriteBorder } from"react-icons/md";
+import { MdFavoriteBorder } from 'react-icons/md';
 
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg';
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes=0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null)
-  const [show, setShow] =  useState(false)
+  const [show, setShow] = useState(false)
 
   useEffect(function () {
-    const observer = new window.IntersectionObserver(function(entries)
-    {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        console.log(' si ' )
-        setShow(true)
-        observer.disconnect()
-      }
-      
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
     })
-    observer.observe(element.current)
-    }, [element])
-     
+  }, [element])
 
   return (
-    <Article ref = {element}>
+    <Article ref={element}>
       {
         show && <Fragment>
           <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src = {src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-      <MdFavoriteBorder size='32' /> {likes} likes!
-      </Button>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
         </Fragment>
       }
-      
     </Article>
   )
 }
