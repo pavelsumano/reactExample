ed3af29b87b949b0bfb4706899e6ab9f73758b6d
pavel <sumanoop@gmail.com>
2019-09-05 23:10:37 -0500

    hoc's

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { GlobalStyle } from './styles/GlobalStyles';
 
 import { ListOfCategories } from './components/ListOfCategories';
-import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { ListOfPhotoCards } from './container/ListOfPhotoCards';
 import { Logo } from './components/Logo';
 
 export const App = () => (
@@ -11,6 +11,6 @@ export const App = () => (
     <GlobalStyle />
     <Logo />
     <ListOfCategories />
-    <ListOfPhotoCards />
+    <ListOfPhotoCards categoryId={1} />
   </div>
 )
