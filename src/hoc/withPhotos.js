8322ef18796065cbf4c7abd99e561cc6a946b5d7
pavel <sumanoop@gmail.com>
2019-09-09 22:24:46 -0500

    Refactor variables querys

--- src/hoc/withPhotos.js
+++ src/hoc/withPhotos.js
@@ -2,8 +2,7 @@ import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
 
-
-export const withPhotos = graphql(gql`
+const GET_PHOTOS = gql`
 query getPhotos($categoryId: ID) {
   photos(categoryId: $categoryId) {
     id
@@ -14,4 +13,5 @@ query getPhotos($categoryId: ID) {
     liked
   }
 }
-`)
+`
+export const withPhotos = graphql(GET_PHOTOS)
