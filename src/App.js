10fc017856b4a9aa4ede7d9a327f68a1e3fed40b
pavel <sumanoop@gmail.com>
2019-09-09 22:09:04 -0500

    render Props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { GlobalStyle } from './styles/GlobalStyles';
 
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './container/ListOfPhotoCards';
 import { Logo } from './components/Logo';
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
 
-export const App = () => (
-  <div>
+  return (
+    <div>
     <GlobalStyle />
     <Logo />
+    {
+      detailId ? <PhotoCardWithQuery id={ detailId } /> :<Fragment>
     <ListOfCategories />
     <ListOfPhotoCards categoryId={1} />
+      </Fragment>
+    }
+
   </div>
-)
+  )
+}
