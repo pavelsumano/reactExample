16dc29aab4186fa71ed49cfbc3958b5626283edd
pavel <sumanoop@gmail.com>
2019-09-12 18:41:01 -0500

    Refactor Detail with Link reach Router

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,32 +1,33 @@
-import React, { Fragment,} from 'react';
+import React, { Fragment } from 'react';
 import { Article, ImgWrapper, Img } from './styles';
 
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearScreen';
 
-import { FavButton } from "../FavButton";
-
-const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+import { FavButton } from '../FavButton';
+import { Link } from '@reach/router';
+const DEFAULT_IMAGE =
+  'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
-  const handleFavClick = () => setLiked(!liked)
+  const [show, element] = useNearScreen();
+  const key = `like-${id}`;
+  const [liked, setLiked] = useLocalStorage(key, false);
+  const handleFavClick = () => setLiked(!liked);
 
   return (
     <Article ref={element}>
-      {
-        show && <Fragment>
-          <a href={`/?detail=${id}`}>
+      {show && (
+        <Fragment>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
 
           <FavButton liked={liked} likes={likes} onclick={handleFavClick} />
         </Fragment>
-      }
+      )}
     </Article>
-  )
-}
+  );
+};
