322636b45d5242c4f5473974d0b5a0edfe876d0c
pavel <sumanoop@gmail.com>
2019-09-11 21:40:30 -0500

    mutaciones likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,9 @@
 import React, { Fragment,} from 'react';
-import { Article, ImgWrapper, Img, Button } from './styles';
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
+import { Article, ImgWrapper, Img } from './styles';
+
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearScreen';
+import { FavButton } from "../FavButton";
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,9 +11,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleFavClick = () => setLiked(!liked)
 
   return (
     <Article ref={element}>
@@ -24,9 +23,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={()=> setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+          <FavButton liked={liked} likes={likes} onclick={handleFavClick} />
         </Fragment>
       }
     </Article>
