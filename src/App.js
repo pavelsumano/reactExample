7743d09a1aa029779d4c95fcad57174df8183cca
pavel <sumanoop@gmail.com>
2019-09-14 12:04:55 -0500

    Rutas Protegidas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import { GlobalStyle } from './styles/GlobalStyles';
-
 import { Logo } from './components/Logo';
+import { NavBar } from './components/NavBar';
+
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
+
 import { Router } from '@reach/router';
-import { NavBar } from './components/NavBar';
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
+
 export const App = () => {
   return (
     <div>
@@ -17,6 +26,23 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path='/favs' />
+              <User path='/user' />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path='/favs' />
+              <NotRegisteredUser path='/user' />
+            </Router>
+          )
+        }
+      </UserLogged>
+
       <NavBar />
     </div>
   );
