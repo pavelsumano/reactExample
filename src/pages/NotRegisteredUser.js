ca0273be4531743bbb379c353326ce9b49bbfaed
pavel <sumanoop@gmail.com>
2019-09-18 19:16:48 -0500

    Login Mutation

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Context from '../Context';
 import { UserForm } from '../components/UserForm';
 import { RegisterMutation } from '../container/RegisterMutation';
+import { LoginMutation } from '../container/LoginMutation';
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
@@ -14,12 +15,37 @@ export const NotRegisteredUser = () => (
                 const input = { email, password };
                 const variables = { input };
                 register({ variables }).then(activateAuth);
-              }
-              const errorMsg = error && 'EL usuario ya existe o hay algun problema';
-              return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />;
+              };
+              const errorMsg =
+                error && 'El usuario ya existe o hay algun problema';
+              return (
+                <UserForm
+                  disabled={loading}
+                  error={errorMsg}
+                  title='Registrarse'
+                  onSubmit={onSubmit}
+                />
+              );
             }}
           </RegisterMutation>
-          <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+          <LoginMutation>
+            {(login, { data, loading, error }) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                login({ variables }).then(activateAuth);
+              };
+              const errorMsg = error && 'el usuario no existe';
+              return (
+                <UserForm
+                  disabled={loading}
+                  error={errorMsg}
+                  title='Iniciar Sesión'
+                  onSubmit={onSubmit}
+                />
+              );
+            }}
+          </LoginMutation>
         </Fragment>
       );
     }}
