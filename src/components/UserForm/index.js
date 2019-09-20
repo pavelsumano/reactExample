fc94ba3ce40c479aec6cdb3a2d05a26325521e5b
pavel <sumanoop@gmail.com>
2019-09-19 22:42:13 -0500

    Close session

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 
 import { useInputValue } from '../../hooks/useInputValue';
-import { Error, Form, Button, Input, Title } from './styles';
-
+import { Error, Form, Input, Title } from './styles';
+import { SubmitButton } from '../SubmitButton';
 export const UserForm = ({ error, disabled, onSubmit, title }) => {
   const email = useInputValue('');
   const password = useInputValue('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
     onSubmit({
       email: email.value,
@@ -20,8 +20,13 @@ export const UserForm = ({ error, disabled, onSubmit, title }) => {
       <Form disabled={disabled} onSubmit={handleSubmit}>
         <Title>{title}</Title>
         <Input disabled={disabled} placeholder='Email' {...email} />
-        <Input disabled={disabled} placeholder='Password' type='password' {...password} />
-        <Button disabled={disabled} >{title}</Button>
+        <Input
+          disabled={disabled}
+          placeholder='Password'
+          type='password'
+          {...password}
+        />
+        <SubmitButton disabled={disabled}>{title}</SubmitButton>
       </Form>
       {error && <Error>{error}</Error>}
     </Fragment>
