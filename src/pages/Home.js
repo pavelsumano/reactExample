1d290334c98eec132f58c186cb4e3265e3bf140e
pavel <sumanoop@gmail.com>
2019-09-22 23:31:43 -0500

    React Helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { ListOfCategories } from '../components/ListOfCategories';
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards';
+import { Layout } from '../components/Layout';
 
 export const Home = ({ id }) => {
   return (
-    <Fragment>
+    <Layout
+      title='Petgram - Tu app de fotos de mascotas'
+      subtitle='Con Petgram puedes encontrar fotos de animales domésticos'
+    >
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-    </Fragment>
+    </Layout>
   );
 };
