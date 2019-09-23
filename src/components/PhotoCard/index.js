8b1379b4ece204ca0255ed5f1b6dfe57d343c9a7
pavel <sumanoop@gmail.com>
2019-09-23 14:34:41 -0500

    PropTypes for validate props

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -7,7 +7,7 @@ import { FavButton } from '../FavButton';
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation';
 
 import { Link } from '@reach/router';
-
+import PropTypes from 'prop-types';
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
@@ -27,10 +27,11 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
           <ToggleLikeMutation>
             {toggleLike => {
               const handleFavClick = () => {
-                  toggleLike({ variables: {
-                      input: { id }
-                    }
-                  });
+                toggleLike({
+                  variables: {
+                    input: { id }
+                  }
+                });
               };
 
               return (
@@ -46,4 +47,18 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       )}
     </Article>
   );
+};
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function(props, propName, componentName) {
+    if (propValue === undefined) {
+      return new Erro(`${propName} value must be defided`);
+    }
+    if (propValue < 0) {
+      return new Error(`${propName} value must be greater than 0`);
+    }
+  }
 };
